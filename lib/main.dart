74de90f45e97349afda6e3e56e0936274f08d91b
abovegore <abovegore400@hotmail.com>
2021-06-20 18:41:25 -0500

    Preparando para Conectar a Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mdb_flutter/src/pages/HomePage.dart';
 import 'package:mdb_flutter/src/routes/routes.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +20,23 @@ class MyApp extends StatelessWidget {
         print('Ruta : ${settings.name}');
         return MaterialPageRoute(builder: (BuildContext context) => HomePage());
       },
+      home: FutureBuilder(
+        future: _fbApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print('Tienes un error! ${snapshot.error.toString()}');
+            return Text('Algo salio mal!');
+          } else if (snapshot.hasData) {
+            return HomePage(
+                //Esto se tiene que cambiar a la pag principal que usamos
+                );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
